cb263bfd1c5406d2d030f05bc349828e31173e6e
shitong01 <stchin@ntu.edu.sg>
2019-09-16 10:37:47 +0000

    updated abckup script

--- command_backup.sh
+++ command_backup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-/home/ops/sciflo/backup_scripts/elasticsearch-backup/backup_custom.py /home/ops/sciflo/backup_scripts/backup_slc_opds_only grq_v1.1_s1-iw_slc '{"query":{"bool":{"must":[{"term":{"metadata.tags.raw":"opendataset"}}]}}}'
-aws s3 sync /home/ops/sciflo/backup_scripts/backup_slc_opds_only s3://ntu-hysds-code/OPDS_SLC_Backup
+set -ex && \
+/home/ops/sciflo/backup_scripts/elasticsearch-backup/backup_custom.py /data/backup_slc_opds_only grq_v1.1_s1-iw_slc '{"query":{"bool":{"must":[{"term":{"metadata.tags.raw":"opendataset"}}]}}}' && \
+aws s3 sync /data/backup_slc_opds_only/ s3://ntu-hysds-code/OPDS_SLC_Backup/ && \
+/home/ops/sciflo/backup_scripts/elasticsearch-backup/backup_all.py /data/backup && \
+aws s3 sync /data/backup/ s3://ntu-hysds-code/ALL_Backup/
