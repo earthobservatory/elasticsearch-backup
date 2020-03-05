5f996445e3fb2415b1fca277c1e34a7deac6a965
shitong01 <stchin@ntu.edu.sg>
2020-03-05 15:19:52 +0800

    better checks

--- migrate_buckets.py
+++ migrate_buckets.py
@@ -66,7 +66,14 @@ def migrate_buckets(from_bucket, to_bucket, backup_dir, target_grq_ip, dry_run=T
 
                         r = requests.put('http://%s:9200/%s/_settings' % (target_grq_ip, idx), data=json.dumps(s))
                         print("Updated settings for %s " % idx)
-                    doctype = dt
+
+                    if "default" not in dt:
+                        doctype = dt
+                        print("Updated datatype from mapping as %s " % doctype)
+
+                if doctype is None:
+                    raise RuntimeError("Failed to find doctype for index %s." % idx)
+
 
             dataset_md = json.loads(l)
             old_prod_url = ""
