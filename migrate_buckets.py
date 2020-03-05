d6c17f35e456e2aba9e47a448064b0c18c915bc3
shitong01 <stchin@ntu.edu.sg>
2020-03-05 15:20:40 +0800

    making sure all is good

--- migrate_buckets.py
+++ migrate_buckets.py
@@ -120,7 +120,7 @@ def migrate_buckets(from_bucket, to_bucket, backup_dir, target_grq_ip, dry_run=T
             es_put_url = 'http://%s:9200/%s/%s/%s' % (target_grq_ip, idx, doctype, dataset_md[id_key])
             print("Putting metadata into %s " % es_put_url)
             if not dry_run:
-                r = requests.put(es_put_url, data=l)
+                r = requests.put(es_put_url, data=json.dumps(dataset_md))
                 print("Updated ES metadata: %s " % dataset_md[id_key])
                 if r.status_code != 201:
                     print(r.status_code)
